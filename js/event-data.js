cb60977fde199bd1eb93a6a94df97ba1e24cf042
Matt Batman <batman.matt@gmail.com>
2022-08-01 19:55:49 -0400

    Add data point

--- js/event-data.js
+++ js/event-data.js
@@ -2,7 +2,13 @@ const data = [
 	{
 		date: '18 Apr 1775',
 		eventName: "Paul Revere's Midnight Ride",
-		eventDescription: "Paul Revere rode to Lexington, MA, to warn of imminent British invasion.",
+		eventDescription: "Paul Revere rode from Charlestown (Boston) to Lexington, MA, to warn of British troop movement. Americans believed the British had orders to arrest John Hancock and Samuel Adams. The British planned to march to Concord, MA, to seize an arms stockpile.",
+		sharedOrPersonal: "Shared"
+	},
+	{
+		date: '19 Apr 1775',
+		eventName: "The Shot Heard Round the World",
+		eventDescription: "British troops met American militia around Lexington, MA. The American militia started to disperse. A shot was fired, and though it was unclear who fired the shot or whether it was intentional, the two sides started fighting.",
 		sharedOrPersonal: "Shared"
 	},
 	{
