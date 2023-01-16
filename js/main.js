678d69384b6f811152d4cfb9e05884bed10806c2
Matt Batman <batman.matt@gmail.com>
2023-01-15 20:43:53 -0500

    Start double-sided timeline via Plot

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
 import '../styles/main.css';
 
-import { timeline } from './timeline';
+// import { timeline } from './timeline';
+import { timelinePlot } from './timeline-plot';
 
 function main() {
-  const americanRevolutionTimeline = timeline();
-  americanRevolutionTimeline.draw();
+  // const americanRevolutionTimeline = timeline();
+  // americanRevolutionTimeline.draw();
+  const chart = document.getElementById('timeline');
+
+  chart.append(timelinePlot())
 }
 
 main();
